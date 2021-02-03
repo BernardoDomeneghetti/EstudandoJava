0096e676e4b7cb1b7461dea7ee22213c49b551bf
Bernardo <BernardoDomeneghetti@gmailcom>
2021-02-03 13:50:42 -0300

    Introdução a generics

--- Java aprendendo/09 - Arrays de tipos dinâmicos/src/base/Teste.java
+++ Java aprendendo/09 - Arrays de tipos dinâmicos/src/base/Teste.java
@@ -1,11 +1,21 @@
 package base;
 
 import arrays_09.ArraysContas;
+import arrays_09.ListasContas;
 
 public class Teste {
 	public static void main(String[] args) {
 		ArraysContas arrCs = new ArraysContas();
 		arrCs.AddContaCorrente(new ContaCorrente(1000, "Bernardo", 1, 1000));
 		arrCs.printaContas();
+		
+		
+		ListasContas lstCs = new ListasContas();
+		lstCs.add(new ContaCorrente(1000, "Bernardo", 1, 1000));
+		lstCs.add(new ContaCorrente(1000, "Eduarda", 1, 1000));
+		
+		System.out.println(lstCs.toString());
+		
+		
 	}
 }
