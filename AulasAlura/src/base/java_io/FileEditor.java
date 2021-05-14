17a28387e1b822e79fb39f18fb0fc15e5adde45e
BernardoDomeneghetti <BernardoDomeneghetti>
2021-05-13 22:20:47 -0300

    File editor creation

--- AulasAlura/src/base/java_io/FileEditor.java
+++ AulasAlura/src/base/java_io/FileEditor.java
@@ -1,13 +1,55 @@
 package base.java_io;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.io.Writer;
 
 public class FileEditor {
+	
+	public static FileEditor newFE() {
+		return new FileEditor();
+	}
+	
+	public String getUserEntranceString() throws IOException {
+		InputStream is = System.in;		
+		Reader r = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(r);
+				
+		String userEntrance = br.readLine();		
+		String resultText = userEntrance;
+		
+		while (userEntrance != null && !userEntrance.isEmpty()) {
+			userEntrance = br.readLine();
+			resultText = userEntrance;
+		}
+		
+		return resultText;
+		
+	}
+	
+	public String getFileEntranceString(String fileAddress) throws IOException {
+		InputStream is = new FileInputStream(fileAddress);		
+		Reader r = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(r);
+				
+		String userEntrance = br.readLine();		
+		String resultText = userEntrance;
+		
+		while (userEntrance != null && !userEntrance.isEmpty()) {
+			userEntrance = br.readLine();
+			resultText = userEntrance;
+		}		
+		return resultText;		
+	}
+	
 	public void writeLineInExistingFile(String fileName, String text) throws IOException {
 		OutputStream fos = new FileOutputStream(fileName);
 		Writer osw = new OutputStreamWriter(fos);
@@ -18,12 +60,16 @@ public class FileEditor {
 		bw.close();
 	}
 	
-	public void writeNewLineInExistingFile(String fileName, String text) throws IOException {
+	public void writeTextInExistingFile(String fileName, String text) throws IOException {
 		OutputStream fos = new FileOutputStream(fileName);
 		Writer osw = new OutputStreamWriter(fos);
-		BufferedWriter bw = new BufferedWriter(osw);				
-		bw.newLine();
-		bw.write(text);
-		bw.close();			
+		BufferedWriter bw = new BufferedWriter(osw);
+				
+//		while ()				
+//		bw.write(text);
+//				
+//		bw.close();
 	}
+	
+
 }
--- AulasAlura/src/runing/Testing.java
+++ AulasAlura/src/runing/Testing.java
@@ -1,17 +1,21 @@
 package runing;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import base.java_io.FileEditor;
 
 public class Testing {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		System.out.println("Insert the text you want to grave in the text file");
+		String teste = FileEditor.newFE().getUserEntranceString();
+			
 		FileEditor fe = new FileEditor();		
 			try {
-				fe.writeLineInExistingFile("ArquivoDeTeste", "Testando escrita em arquivo de texto");
+				fe.writeLineInExistingFile("ArquivoDeTeste", teste);
+				
 			} catch (IOException e) {
 				e.printStackTrace();
-			}
-		
+			}		
 	}
 }
